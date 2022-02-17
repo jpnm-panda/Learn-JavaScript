680114b685d935bd653a1701934c4d4a54c28385
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-17 09:57:46 +0900

    関数のおさらい

--- 2022-02-17/sample.js
+++ 2022-02-17/sample.js
@@ -39,7 +39,47 @@
 // val4.address = "Ueno-Zoo";
 // console.log(val4);
 
-const val5 = ['dog', 'cat'];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/* 
+ テンプレート文字列
+*/
+const name = "Panda";
+const age = 5;
+// 「私の名前はPandaです。年齢は5歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
+
+/* 
+ アロー関数
+*/
+
+// 従来の関数
+// function func1(str) {
+//     return str;
+// }
+const func1 = function (str) {
+    return str;
+};
+
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+    return str;
+};
+
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+
+console.log(func3(10, 30));
