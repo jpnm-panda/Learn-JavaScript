e0e0940d98885857f332a4d053024203f4407d3c
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-17 09:24:32 +0900

    変数定義をおさらい

---
+++ 2022-02-17/sample.js
@@ -0,0 +1,45 @@
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// let val2 = 'let変数を再宣言';
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const val4 = {
+//     name: "Panda",
+//     age: "3",
+// };
+
+// console.log(val4);
+
+// val4.name = "Panda-san";
+
+// console.log(val4);
+
+// val4.address = "Ueno-Zoo";
+// console.log(val4);
+
+const val5 = ['dog', 'cat'];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
