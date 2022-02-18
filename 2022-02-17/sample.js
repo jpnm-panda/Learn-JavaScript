d6f50400225b991afbfa815b36fda87c2334e94a
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-18 10:34:09 +0900

    filterとmapを勉強

--- 2022-02-17/sample.js
+++ 2022-02-17/sample.js
@@ -104,12 +104,92 @@
 
 // console.log(message2);
 
-const myProfile = ['Panda', 5];
+// const myProfile = ['Panda', 5];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
+// const [name, age] = myProfile;
 
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など 
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("Panda");
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 入れるのコピーや結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
+
+// arr9 = [...arr4];
+// arr9[0] = 10;
+// console.log(arr9);
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけえ"];
+
+// for文を使う場合
+// for (let index = 0; index < nameArr.length; index++) {
+//     console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// mapの基本
+// const nameArr2 = nameArr.map((name) => {
+//     return name;
+// })
+// console.log(nameArr2);
+
+// mapを使う場合
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// filter をつかう
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//     return num % 2 === 0
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//     if (name === "Panda") {
+//         return name
+//     } else {
+//         return `${name}さん`
+//     }
+// })
+// console.log(newNameArr);
