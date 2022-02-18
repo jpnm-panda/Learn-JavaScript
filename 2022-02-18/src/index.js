e7c51a8ed805fe319bfeadc44f129a0c9f632575
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-19 08:18:27 +0900

    入力内容を変数に代入したliタグ生成機能を作った

--- 2022-02-18/src/index.js
+++ 2022-02-18/src/index.js
@@ -4,7 +4,20 @@ const onClickAdd = () => {
     // テキストボックスの値を取得し、初期化する
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
-    alert(inputText);
+
+    // liタグ生成
+    const li = document.createElement("li");
+    li.className = "list-row";
+
+    // pタグ生成
+    const p = document.createElement("p");
+    li.innerText = inputText;
+
+    // liタグの子要素に各要素を設定
+    li.appendChild(p);
+
+    // 未完了リストに追加
+    document.getElementById("incomplete-list").appendChild(li);
 };
 
 document
