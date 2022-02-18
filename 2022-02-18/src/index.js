1afdd43f1d36a15d4f2c8798b81ac556ef9a3ee0
jpnm-panda <jpnm0415shkm@gmail.com>
2022-02-19 08:31:52 +0900

    完了ボタンと削除ボタンが生成されるように調整

--- 2022-02-18/src/index.js
+++ 2022-02-18/src/index.js
@@ -13,8 +13,24 @@ const onClickAdd = () => {
     const p = document.createElement("p");
     li.innerText = inputText;
 
+    // button(完了)タグを生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        alert("完了");
+    });
+
+    // button(削除)タグを生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+        alert("削除");
+    });
+
     // liタグの子要素に各要素を設定
     li.appendChild(p);
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
 
     // 未完了リストに追加
     document.getElementById("incomplete-list").appendChild(li);
